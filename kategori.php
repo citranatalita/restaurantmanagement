e5b4ef6696554d7943cd1b119583243c05743f4a
sandra-yani <sandra.april29@smk.belajar.id>
2025-01-14 14:59:36 +0700

    update kategory,dan menambahkan proses kategory

--- kategori.php
+++ kategori.php
@@ -1,20 +1,22 @@
-<i?php
-// Memasukkan header halaman
+<?php
+
 include '.includes/header.php';
-// Menyertakan file untuk menampilkan notifikasi (jika ada)
+
 include '.includes/toast_notification.php';
 ?>
 
 <div class="container-xxl flex-grow-1 container-p-y">
-<!-- Tabel data kategori -->
-  <div class="card">
-    <div class="card-header d-flex justify-content-between align-items-center">
-      <h4>Data Kategori</h4>
-<!-- Tombol untuk menambah kategori baru -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCategory">
-Tambah Kategori
-      </button>
-    </div>
+
+    <div class="card">
+        <div class="card-header d-flex justify-content-between 
+align-items-center">
+    <h4>Data Kategori</h4>
+
+    <button type="button" class="btn btn-primary" data-bs-
+toggle="modal" data-bs-target="#addCategory">
+        Tambah Kategori
+    </button>
+  </div>
 
 <div class="card-body">
   <div class="table-responsive text-nowrap">
@@ -26,45 +28,74 @@ Tambah Kategori
           <th width="150px">Pilihan</th>
         </tr>
       </thead>
-      <tbody class="table-border-bottom-8"></tbody>
+      <tbody class="table-border-bottom-0">
 
       <?php
       $index = 1;
       $query = "SELECT * FROM categories";
       $exec = mysqli_query($conn, $query);
-      while ($category = mysqli_fetch_assoc($exec))
+      while ($category = mysqli_fetch_assoc($exec)) :
       ?>
-      <tr>
+     <tr>
 
-        <td><?= $index++; ?></td>
-        <td><?= $category['category_name']; ?></td>
-        <td>
+     <td><?= $index++; ?></td>
+     <td><?= $category['category_name']; ?></td>  
+<td>
 
-          <div class="dropdown">
-            <button type="button" class="btn p-0 dropdown-toggle
-            hide-arrow" data-bs-toggle="dropdown">
-              <i class="bx bx-dots-vertical-rounded"></i>
-            </button>
-            <div class="dropdown-menu">
-              <a href="#" class="dropdown-item" data-bs-toggle="modal"
-              data-bs-target="#editCategory_<?= $category['category_id']; ?>">
+    <div class="dropdown">
+        <button type="button" class="btn p-0 dropdown-toggle
+        hide-arrow" data-bs-toggle="dropdown">
+            <i class="bx bx-dots-vertical-rounded"></i>
+        </button>
+        <div class="dropdown-menu">
+            <a href="#" class="dropdown-item" data-bs-toggle="modal"
+            data-bs-target="#editCategory_<?= $category['category_id']; ?>">
                 <i class="bx bx-edit-alt me-2"></i> Edit </a>
-              <a href="#" class="dropdown-item" data-bs-toggle="modal"
-              data-bs-target="#deleteCategory_<?= $category['category_id']; ?>">
-                <i class="bx bx-trash me-2"></i> Delete </a>
-            </div>
-          </div>
-        </td>
+            <a href="#" class="dropdown-item" data-bs-toggle="modal"
+            data-bs-target="#deleteCategory_<?= $category['category_id']; ?>">
+        <i class="bx bx-trash me-2"></i> Delete </a>
+    </div>
+  </div>
+</td>
+            </tr>
 
-        </tr>
 
 
 
             <?php endwhile; ?>
           </tbody>
-        </table>
+        </tables>
       </div>
     </div>
   </div>
 </div>
 <?php include '.includes/footer.php'; ?>
+
+<div class="modal fade" id="addCategory" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Tambah Data</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal">
+</button>
+      </div>
+      <div class="modal-body">
+        <form action="proses_kategori.php" method="POST">
+            <div>
+                <label for="namaKategori" class="form-label">Nama
+Kategori</label>
+
+            <input type="text" class="form-control" name="category_name"
+required/>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary"
+                data-bs-dismiss="modal">Batal</button>
+                <button type="submit" name="simpan" class="btn btn-
+primary">Simpan</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
