553a1f3e5e3210eb607b6ee01a2c070d11a6df2f
sandra-yani <sandra.april29@smk.belajar.id>
2025-02-04 11:37:50 +0700

    menambahkan sebagian halaman dashboard

---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<?php
+include (".includes/header.php");
+$title = "Dashboard";
+// Menyertakan file untuk menampilkan notifikasi (jika ada)
+include '.includes/toast_notification.php';
+?>
+<div class="container-xxl flex-grow-1 container-p-y">
+    <!-- Card untuk menampilkan tabel postingan -->
+    <div class="card">
+        <!-- Tabel dengan baris yang dapat di-hover -->
+        <div class="card">
+            <!-- Header Tabel -->
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <h4>Semua Postingan</h4>
+            </div>
+            <div class="card-body">
+                <!-- Tabel responsif -->
+                <div class="table-responsive text-nowrap">
+                    <table id="datatable" class="table table-hover">
